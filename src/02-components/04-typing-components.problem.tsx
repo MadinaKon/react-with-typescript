bfc781ac49c8ee525eee9a9ab18249c0d94dd052
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-12-08 16:25:50 +0100

    make className property optional and add type PropsType

--- src/02-components/04-typing-components.problem.tsx
+++ src/02-components/04-typing-components.problem.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-export const Button = (props: {className: string}) => {
+type PropsType = { className?: string };
+export const Button = (props: PropsType) => {
   return <button className={props.className}></button>;
 };
 
 const Parent = () => {
   return (
     <>
-      {/* @ts-expect-error */}
       <Button></Button>
 
       <Button className="my-class"></Button>
